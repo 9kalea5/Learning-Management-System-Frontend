2e2b34de817ad892231cb27f34f04b3639ab1226
kaleab <alebachewkaleab@gmail.com>
2025-06-22 19:05:55 +0300

    Registration Successfull

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -4,12 +4,12 @@ import jwtDecode from "jwt-decode"
 import Cookie from "js-cookie"
 import Swal from "sweetalert2"
 
-export default async function login(email, password) {
+export default async function login(email, password){
     try {
         const {data, status} = await axios.post(`user/token/`, {
             email,
             password,
-        });
+        })
         if (status===200){
             setAuthUser(data.access, data.refresh)
             alert("Login Successfull")
@@ -22,5 +22,22 @@ export default async function login(email, password) {
             data: null,
             error: error.response.data?.detail || "Something Went Wrong"
         }
+    }
+}
+
+export default async function register(full_name, email, password, password2){
+    try {
+        const {data} = await axios.post(`user/register`, {
+            full_name,
+            email,
+            password,
+            password2,
+        })
+        await login(email, password)
+        alert("Registration Successfull")
+        return {data, error:null}
+    }
+    catch (error){
+
     }
 }

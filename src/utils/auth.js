abbdb4481360fadb197f6bd1fa4ce1f852583525
kaleab <alebachewkaleab@gmail.com>
2025-06-22 18:45:54 +0300

    Authentication Successfull

---
+++ src/utils/auth.js
@@ -0,0 +1,21 @@
+import {useAuth} from "../store/auth"
+import axios from "./axios"
+import jwtDecode from "jwt-decode"
+import Cookie from "js-cookie"
+import Swal from "sweetalert2"
+
+export default async function login(email, password) {
+    try {
+        const {data, status} = await axios.post(`user/token/`, {
+            email,
+            password,
+        });
+        if (status===200){
+            setAuthUser(data.access, data.refresh)
+            alert("Login Successfull")
+        }
+    }
+    catch (error){
+        console.error("Login failed: ", error)
+    }
+}

7a8082911777289d3d43f8ec4f13ef7a9ccc5721
kaleab <alebachewkaleab@gmail.com>
2025-06-24 09:12:14 +0300

    Private routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,41 @@
-import { useState } from 'react'
-import {Routes, Route} from 'react-router-dom'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { AuthProvider } from './store/auth';
+import { MainWrapper } from './components/MainWrapper';
+import { PrivateRoutes } from './components/PrivateRoutes';
+import RegisterPage from './pages/RegisterPage';
+import LoginPage from './pages/LoginPage';
+import Dashboard from './pages/Dashboard';
 
 function App() {
-
   return (
-    <>
-    </>
-  )
+    <Router>
+      <AuthProvider>
+        <MainWrapper>
+          <Routes>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route
+              path="/"
+              element={
+                <PrivateRoutes>
+                  <Navigate to="/dashboard" replace />
+                </PrivateRoutes>
+              }
+            />
+            <Route
+              path="/dashboard"
+              element={
+                <PrivateRoutes>
+                  <Dashboard />
+                </PrivateRoutes>
+              }
+            />
+          </Routes>
+        </MainWrapper>
+      </AuthProvider>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- src/layouts/PrivateRoutes.jsx
+++
@@ -1,8 +0,0 @@
-import {Navigate} from 'react-router-dom'
-import {useAuth} from "../store/auth"
-
-export default function PrivateRoute(children){
-    const loggedIn = useAuth((state) => state.isLoggedIn)()
-
-    return loggedIn ? <>{children}</> : <Navigate to="/login/" />
-}

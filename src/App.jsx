1caefd64281955c678c2a15f0768e4363439c09f
kaleab <alebachewkaleab@gmail.com>
2025-06-24 09:02:16 +0300

    Private Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,10 @@
 import { useState } from 'react'
-import Login from './pages/Login'
-import Home from './components/LandingPage'
-import Register from './pages/Register'
 import {Routes, Route} from 'react-router-dom'
 
 function App() {
 
   return (
     <>
-      <Routes>
-        <Route path='/login' element={<Login />}/>
-        <Route path='/register' element={<Register />}/>
-        <Route path='/' element={<Layout />}/>
-      </Routes>
     </>
   )
 }

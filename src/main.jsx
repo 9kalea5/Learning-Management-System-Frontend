9a2c1919713cae05b19f30fd8aa5a4edaaeb752f
kaleab <alebachewkaleab@gmail.com>
2025-07-17 11:56:55 +0300

    Tailwind

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,9 @@
-import { React, StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <Router>
-      <StrictMode>
-        <App />
-      </StrictMode>,
-  </Router>
-
+import './index.css'
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
